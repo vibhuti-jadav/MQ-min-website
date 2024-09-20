b48e75cba84d9ec22aacb45e668ead9630dd667b
vibhuti-jadav <vibhutij109@gmail.com>
2024-09-20 17:50:29 +0530

    updatedd

--- js/script.js
+++ js/script.js
@@ -29,4 +29,33 @@ $('.autoplay').slick({
         // instead of a settings object
       ]
   });
+
+  $('.btnclick').click(function(){
+    $('.side-nav').css('width','300px')
+  })
+
+
+  $('.btnclick1').click(function(){
+    $('.side-nav').css('width','0px')
+  })
+
+  $('.d1').click(function(){
+    $('.m1').toggle();
+  })
       
+  $('.d2').click(function(){
+    $('.m2').toggle();
+  })
+      
+  $('.d3').click(function(){
+    $('.m3').toggle();
+  })
+      
+  $('.d4').click(function(){
+    $('.m4').toggle();
+  })
+    
+  $('.d5').click(function(){
+    $('.m5').toggle();
+  })
+    
